bd53b976f760301f45f085e57d47239e4e2332aa
minjekim12 <101533438+minjekim12@users.noreply.github.com>
2022-05-24 14:57:52 +1200

    feat: added initial comments

---
+++ main.py
@@ -0,0 +1,12 @@
+""" This program allows the group leader to put in details
+ about where they are, number of campers and the current 
+ weather conditions every morning and night. 
+ Then the user can store and display all past informations."""
+
+ # Import everything from Tkinter
+from tkinter import *
+
+# Imports ttk 
+from tkinter import ttk
+
+root = Tk()
